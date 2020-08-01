d5d76d74c03561da5935f8728813e222ff13ffd7
EnglishConnect <66697429+EnglishConnect@users.noreply.github.com>
2020-07-31 19:12:05 -0500

    tincan

---
+++ tincan/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://l6Vk8Y8dmUS5ZhOaS0dr_90qKBfphFcF_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "EnglishConnect 1"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;&lt;span style=&#34;color: rgb(36, 103, 178); font-size: 28px;&#34;&gt;Otros Recursos&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;a target=&#34;_blank&#34; rel=&#34;noopener noreferrer&#34; href=&#34;mailto:englishconnectrobot@gmail.com&#34;&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;a href=&#34;https://englishconnect.github.io/spa/2/&#34; rel=&#34;noopener noreferrer&#34; target=&#34;_blank&#34;&gt;EnglishConnect 2&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;a href=&#34;https://englishconnect.github.io/spa/lectura&#34; rel=&#34;noopener noreferrer&#34; target=&#34;_blank&#34;&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;a href=&#34;https://englishconnect.github.io/spa/3/&#34; rel=&#34;noopener noreferrer&#34; target=&#34;_blank&#34;&gt;EnglishConnect 3&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;EnglishConnect curso de prueba para las misiones del area de Dallas Fort Worth&lt;/p&gt;&lt;p&gt;&lt;span style=&#34;font-size: 12px;&#34;&gt;If you are outside the DFW Area please get mission president approval, &lt;a href=&#34;https://docs.google.com/forms/d/e/1FAIpQLScpyUKGTiBO_Y3VmwePTPeqZ0vesskRnFoZUl46n5M48tczbA/viewform?usp=sf_link&#34; rel=&#34;noopener noreferrer&#34; target=&#34;_blank&#34;&gt;send us your email&lt;/a&gt; and review the&amp;nbsp;&lt;/span&gt;&lt;a href=&#34;https://englishconnect.github.io/instructor&#34; rel=&#34;noopener noreferrer&#34; target=&#34;_blank&#34;&gt;&lt;span style=&#34;font-size: 12px;&#34;&gt;training&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;"
+};
